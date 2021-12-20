353fd21d93488a5b2df1618fdbff8d1abe4a4dfd
jhnguwefgtugjwyrgj <46817852+jhnguwefgtugjwyrgj@users.noreply.github.com>
2021-12-20 23:47:13 +0300

    Add files via upload

---
+++ Ryabov_BD.cpp
@@ -0,0 +1,245 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+class Students {
+	string fname;
+	int col=0, row=0;
+	vector <string> head;
+	vector <vector <string> > mat;
+	public:
+		Students(string);
+		void Close();
+		void Insert();
+		void Out();
+		void Change(int n);
+		vector <string> Choose(int n);
+		void NewCol();
+		vector<string> NewMark();
+		int Getcol();
+		int Getrow();
+		string Getfio(int n);
+		void Del(int n);
+};
+
+Students::Students(string name="students.txt") {
+	ifstream Fin(name.c_str());
+	fname = name;
+	string s, cur="";
+	int j=0;
+	getline(Fin, s);
+	if (s.size() == 0){
+	    if (name=="students.txt") {
+    		vector <string> h = {"familiya", "imya", "otchectvo"};
+    		head = h;
+    		col = head.size();
+    	}
+    	if (name=="subjects.txt") {
+    		vector <string> h = {"subject"};
+    		head = h;
+    		col = head.size();
+    	}
+    	if (name=="marks.txt") {
+    		vector <string> h = {"id_st", "id_sub", "mark"};
+    		head = h;
+    		col = head.size();
+    	}
+	}
+	else {
+		s = s.substr(0, s.size());
+		for (size_t i=0;i<s.size();i++) {
+			if (s[i] != ' ') cur += s[i];
+			else {
+				head.push_back(cur);
+				cur="";
+			}
+		}
+		head.push_back(cur);
+		col = head.size();
+		while (!Fin.eof()) {
+			vector<string>r(col, "");
+			for (size_t i=0;i<col;i++) {
+				Fin >> r[i];
+			}
+			mat.push_back(r);
+			row++;
+		}
+	}
+	Fin.close();
+}
+
+void Students::Close() {
+	ofstream Fout;
+	Fout.open(fname.c_str());
+	for (size_t i=0;i<col-1;i++) {
+		Fout << head[i] << " ";
+	}
+	Fout << head[col-1] << endl;
+	for (int i=0;i<row-1;i++) {
+		for (int j=0;j<col;j++) {
+			if (j != col-1) Fout << mat[i][j] << " ";
+			else Fout << mat[i][j] << endl;
+		}
+	}
+	for (int j=0;j<col;j++) {
+			if (j != col-1) Fout << mat[row-1][j] << " ";
+			else Fout << mat[row-1][j];
+		}
+	Fout.close();
+}
+
+void Students::Insert() {
+	vector <string> line(col);
+	for (int i=0;i<col;i++) {
+	    cout << "Print " << head[i] << ": ";
+	    string t;
+	    cin >> t;
+	    line[i] = t;
+	}
+	mat.push_back(line);
+	row++;
+}
+
+void Students::Out() {
+    for (int i=0;i<col;i++) {
+        cout << head[i] << " ";
+    }
+    cout << endl;
+    for (int i=0;i<row;i++) {
+        for (int j=0;j<col;j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Students::Change(int n) {
+    if (n <= row && n>=1) {
+        vector <string> line(col);
+    	for (int i=0;i<col;i++) {
+    	    cout << "Print " << head[i] << ": ";
+    	    string t;
+    	    cin >> t;
+    	    line[i] = t;
+    	}
+    	mat[n-1] = line;
+    }
+    else {
+        cout << "wrong number" << endl;
+    }
+}
+
+vector <string> Students::Choose(int n) {
+    return mat[n-1];
+}
+
+void Students::NewCol() {
+    cout << "Print name column: ";
+    string s;
+    cin >> s;
+    head.push_back(s);
+    for (int i=0;i<row;i++) {
+        mat[i].push_back("");
+    }
+    col++;
+}
+
+int Students::Getrow() {
+    return row;
+}
+
+int Students::Getcol() {
+    return col;
+}
+
+string Students::Getfio(int n) {
+    return mat[n-1][0]+" "+mat[n-1][1]+" "+mat[n-1][2];
+}
+
+void Students::Del(int n) {
+    mat.erase(mat.begin()+(n-1));
+    row--;
+}
+
+vector <string> Students::NewMark() {
+    vector <string> line(3);
+    cout << "Print student id: ";
+    cin >> line[0];
+    cout << "Print subject id: ";
+    cin >> line[1];
+    cout << "Print mark: ";
+    cin >> line[2];
+    mat.push_back(line);
+    return line;
+}
+
+
+int main() {
+	Students pup("students.txt");
+	Students sub("subjects.txt");
+	Students mar("marks.txt");
+	vector <vector <int> > ans, g;
+	vector <string> fio;
+	vector <string> head_ans(1, "fio");
+	cout << "Welcome" << endl;
+	while (true) {
+	    cout << "Enter command (help for command list)" << endl;
+	    string com;
+	    cin >> com;
+	    if (com == "exit") break;
+	    if (com == "add students string") {
+	        pup.Insert();
+	        string t = pup.Getfio(pup.Getrow());
+            fio.push_back(t);
+            ans.push_back(vector<int>(head_ans.size()-1, 0));
+	    }
+	    if (com == "add mark") {
+	        vector <string> e = mar.NewMark();
+	        if (stoi(e[0]) >= 1 && stoi(e[0]) <= pup.Getrow() &&
+	        stoi(e[1]) >= 1 && stoi(e[1]) <= sub.Getrow()) {
+	            ans[stoi(e[0])][stoi(e[1])] += stoi(e[3]);
+	            g[stoi(e[0])][stoi(e[1])]++;
+	        }
+	        else {
+	            cout << "Uncorrect data";
+	            mar.Del(mar.Getrow());
+	        }
+	    }
+	    if (com == "add subject") {
+	        sub.Insert();
+	        head_ans.push_back(sub.Choose(Getrow())[0]);
+	        for (int i=0;i<ans.size();i++) {
+	            ans[i].push_back(0);
+	        }
+	    }
+	    if (com == "change student") {
+	        int k;
+	        cin >> k;
+	        pup.Change(k);
+	    }
+	    if (com == "get request") {
+	        ofstream Fout("request.txt");
+	        for (int i=0;i<head_ans.size();i++) {
+	            Fout << head_ans[i] << " ";
+	        }
+	        Fout << endl;
+	        for (int i=0;i<ans.size();i++) {
+	            Fout << fio[i];
+	            for (int j=0;j<ans[i];j++) {
+	                if (g[i][j] > 0) Fout << ans[i][j]/g[i][j] << " ";
+	                else Fout << 0 << " ";
+	            }
+	            Fout << endl;
+	        }
+	        Fout.close();
+	    }
+	}
+	sub.Close();
+	mar.Close();
+	pup.Close();
+	return 0;
+}
